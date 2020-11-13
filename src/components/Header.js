eb6f4859f0ac00dd119900381db80a70dd5fe498
Elvis020 <elvis.amoako@stu.ucc.edu.gh>
2020-11-13 07:38:13 +0000

    Travel Website with Gatsby

---
+++ src/components/Header.js
@@ -0,0 +1,55 @@
+import { Link } from "gatsby"
+import React from "react";
+import styled from 'styled-components';
+import {FaBars} from 'react-icons/fa';
+
+const Header = () => {
+  return (
+    <Nav>
+      <NavLink to='/'>sPaCeY Explorer</NavLink>
+      <Bars />
+    </Nav>
+  )
+}
+
+export default Header
+
+
+
+// Targetting styled components
+const Nav = styled.nav`
+  background: navy;
+  height: 80px;
+  display: flex;
+  justify-content: space-between;
+  padding: .5rem (calc(100vw-1300px) / 2);
+  z-index: 100;
+  position: relative;
+  
+`
+const NavLink = styled(Link)`
+  color: #fff;
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  padding: 0 1rem;
+  height: 100%
+  cursor: pointer;
+`
+
+
+const Bars = styled(FaBars)`
+  display: none;
+  color: #fff;
+
+  @media screen and(max-width: 768px){
+    display: block;
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(-100%,75%);
+    font-size: 1.8rem;
+    cursor: pointer;
+
+  }
+`
